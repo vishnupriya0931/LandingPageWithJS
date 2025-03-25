6dd6c365cec02cb782641e9140a630e0f7200000
vishnupriya0931 <vishnupriya2631@gmail.com>
2025-03-25 16:56:32 +0530

    landing page

---
+++ index.js
@@ -0,0 +1,103 @@
+// const scrollRevealOption = {
+//     distance: "50px",
+//     origin:"bottom",
+//     duration:1000,
+// };
+
+// scrollReveal().reveal(".container .letter-s" , {
+//     duration:1000,
+//     delay:1000,
+// })
+// scrollReveal().reveal(".container .img" , {
+//     duration:1000,
+//     delay:1500,
+// })
+
+// scrollReveal().reveal(".container .text__left",{
+//     ...scrollRevealOption,
+//     origin:"right",
+//     delay:2000,
+// })
+// scrollReveal().reveal(".container .text__right",{
+//     ...scrollRevealOption,
+//     origin:"left",
+//     delay:2000,
+// })
+// scrollReveal().reveal(".container .explore",{
+//     duration:1000,
+//     delay:2500,
+// })
+// scrollReveal().reveal(".container .catlog",{
+//     duration:1000,
+//     delay:5000,
+// })
+// scrollReveal().reveal(".container .print",{
+//     duration:1000,
+//     delay:5500,
+// })
+// scrollReveal().reveal(".container h5",{
+//     duration:1000,
+//     interval:500,
+//     delay:5500,
+// })
+
+// scrollReveal().reveal(".footer p",{
+//     duration:1000,
+//     delay:700,
+// })
+
+
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".container .letter-s", {
+    duration: 1000,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".container .watch-img", {
+    duration: 1000,
+    delay: 3000,
+});
+
+ScrollReveal().reveal(".container .text__left", {
+    ...scrollRevealOption,
+    origin: "right",
+    delay: 2000,
+});
+
+ScrollReveal().reveal(".container .text__right", {
+    ...scrollRevealOption,
+    origin: "left",
+    delay: 2000,
+});
+
+ScrollReveal().reveal(".container .explore", {
+    duration: 1000,
+    delay: 2500,
+});
+
+ScrollReveal().reveal(".container .catalog", {
+    duration: 1000,
+    delay: 5000,
+});
+
+ScrollReveal().reveal(".container .print", {
+    duration: 1000,
+    delay: 5500,
+});
+
+ScrollReveal().reveal(".container h5", {
+    duration: 1000,
+    interval: 500,
+    delay: 5500,
+});
+
+ScrollReveal().reveal(".footer p", {
+    duration: 1000,
+    delay: 7000,
+});
