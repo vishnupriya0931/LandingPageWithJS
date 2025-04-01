69f4df540eface629dbf77df348ef4591d127d54
Diswanthkumar26 <diswanthkumar.ch@gmail.com>
2025-04-01 12:46:12 +0530

    updated nav

--- index.js
+++ index.js
@@ -1,53 +1,3 @@
-// const scrollRevealOption = {
-//     distance: "50px",
-//     origin:"bottom",
-//     duration:1000,
-// };
-
-// scrollReveal().reveal(".container .letter-s" , {
-//     duration:1000,
-//     delay:1000,
-// })
-// scrollReveal().reveal(".container .img" , {
-//     duration:1000,
-//     delay:1500,
-// })
-
-// scrollReveal().reveal(".container .text__left",{
-//     ...scrollRevealOption,
-//     origin:"right",
-//     delay:2000,
-// })
-// scrollReveal().reveal(".container .text__right",{
-//     ...scrollRevealOption,
-//     origin:"left",
-//     delay:2000,
-// })
-// scrollReveal().reveal(".container .explore",{
-//     duration:1000,
-//     delay:2500,
-// })
-// scrollReveal().reveal(".container .catlog",{
-//     duration:1000,
-//     delay:5000,
-// })
-// scrollReveal().reveal(".container .print",{
-//     duration:1000,
-//     delay:5500,
-// })
-// scrollReveal().reveal(".container h5",{
-//     duration:1000,
-//     interval:500,
-//     delay:5500,
-// })
-
-// scrollReveal().reveal(".footer p",{
-//     duration:1000,
-//     delay:700,
-// })
-
-
-
 const scrollRevealOption = {
     distance: "50px",
     origin: "bottom",
@@ -101,3 +51,12 @@ ScrollReveal().reveal(".footer p", {
     duration: 1000,
     delay: 7000,
 });
+
+
+function showSidebar() {
+    document.getElementById("sidebar").classList.add("active");
+}
+
+function hideSidebar() {
+    document.getElementById("sidebar").classList.remove("active");
+}
